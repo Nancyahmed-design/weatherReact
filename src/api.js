609c090831b0f525453f2d7b5bcde9589c8569f8
Nancyahmed-design <nancyahmed2025@gmail.com>
2023-09-22 10:11:50 +0200

    new changes

---
+++ src/api.js
@@ -0,0 +1,13 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b3f5f3568cmshb560fcfe099441ep1a6d28jsn51d6c0f685c8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8cd5c326a2955c944fe6755a46d6da5f";
